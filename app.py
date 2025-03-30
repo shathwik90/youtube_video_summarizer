463a464c6daa603d2015342c62b0b19d3b9bd986
Shathwik Reddy Duvvuru <shathwik90@gmail.com>
2025-03-30 19:54:29 +0530

    Update app to run on configurable port and host

--- app.py
+++ app.py
@@ -121,4 +121,5 @@ def summarize():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(debug=True, host="0.0.0.0", port=port)
